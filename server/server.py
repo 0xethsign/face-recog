59ada64aa41e35c0e8a8b48ba49d7bdcc95f2df3
smrpn <samarpan_d@pp.iitr.ac.in>
2020-08-01 08:17:34 +0530

    ui not added

---
+++ server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask,request,jsonify
+import util.py
+
+app=Flask(__name__)
+
+@app.route('/classify_image',methods=['GET','POST'])
+def hello():
+    return "hi"
+
+if __name__ == "__main__":
+    app.run(port=5000)
+
